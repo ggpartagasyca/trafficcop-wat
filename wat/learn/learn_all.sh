a5fb7c8667e4af6b714f8d35c1482344b98b4e5a
Andrew Philpot <andrew.philpot@gmail.com>
2015-05-28 08:31:41 -0700

    use renamed python

--- wat/learn/learn_all.sh
+++ wat/learn/learn_all.sh
@@ -1,16 +1,31 @@
 #!/bin/sh
 
-echo  age
-python learnblob.py -v -s age --train 800 --test 100 --validate 100
-echo  race
-python learnblob.py -v -s race --train 800 --test 100 --validate 100
-echo  multi
-python learnblob.py -v -s multi --train 800 --test 100 --validate 100
-echo  agency
-python learnblob.py -v -s agency --train 800 --test 100 --validate 100
-echo  healthspa
-python learnblob.py -v -s healthspa --train 800 --test 100 --validate 100
-echo  offtopic
-python learnblob.py -v -s offtopic --train 800 --test 100 --validate 100
-echo  typical
-python learnblob.py -v -s typical --train 800 --test 100 --validate 100
+echo age-cs
+python postclassifier.py -v -s age -i cs --train 800 --test 100 --validate 100
+# echo race-cs
+# python postclassifier.py -v -s race -i cs --train 800 --test 100 --validate 100
+# echo multi-cs
+# python postclassifier.py -v -s multi -i cs --train 800 --test 100 --validate 100
+# echo agency-cs
+# python postclassifier.py -v -s agency -i cs --train 800 --test 100 --validate 100
+# echo healthspa-cs
+# python postclassifier.py -v -s healthspa -i cs --train 800 --test 100 --validate 100
+# echo offtopic-cs
+# python postclassifier.py -v -s offtopic -i cs --train 800 --test 100 --validate 100
+# echo typical-cs
+# python postclassifier.py -v -s typical -i cs --train 800 --test 100 --validate 100
+
+# echo age-ci
+# python postclassifier.py -v -s age -i ci --train 800 --test 100 --validate 100
+# echo race-ci
+# python postclassifier.py -v -s race -i ci --train 800 --test 100 --validate 100
+# echo multi-ci
+# python postclassifier.py -v -s multi -i ci --train 800 --test 100 --validate 100
+# echo agency-ci
+# python postclassifier.py -v -s agency -i ci --train 800 --test 100 --validate 100
+# echo healthspa-ci
+# python postclassifier.py -v -s healthspa -i ci --train 800 --test 100 --validate 100
+# echo offtopic-ci
+# python postclassifier.py -v -s offtopic -i ci --train 800 --test 100 --validate 100
+# echo typical-ci
+# python postclassifier.py -v -s typical -i ci --train 800 --test 100 --validate 100
--- wat/learn/test_all.sh
+++ wat/learn/test_all.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 
 echo  age
-python learnblob.py -v -l age --train 800 --test 100 --validate 100 > age.test
-echo  race
-python learnblob.py -v -l race --train 800 --test 100 --validate 100 > race.test
-echo  multi
-python learnblob.py -v -l multi --train 800 --test 100 --validate 100 > multi.test
-echo  agency
-python learnblob.py -v -l agency --train 800 --test 100 --validate 100 > agency.test
-echo  healthspa
-python learnblob.py -v -l healthspa --train 800 --test 100 --validate 100 > healthspa.test
-echo  offtopic
-python learnblob.py -v -l offtopic --train 800 --test 100 --validate 100 > offtopic.test
-echo  typical
-python learnblob.py -v -l typical --train 800 --test 100 --validate 100 > typical.test
+python postclassifier.py -v -l age --train 800 --test 100 --validate 100 > age.test
+# echo  race
+# python postclassifier.py -v -l race --train 800 --test 100 --validate 100 > race.test
+# echo  multi
+# python postclassifier.py -v -l multi --train 800 --test 100 --validate 100 > multi.test
+# echo  agency
+# python postclassifier.py -v -l agency --train 800 --test 100 --validate 100 > agency.test
+# echo  healthspa
+# python postclassifier.py -v -l healthspa --train 800 --test 100 --validate 100 > healthspa.test
+# echo  offtopic
+# python postclassifier.py -v -l offtopic --train 800 --test 100 --validate 100 > offtopic.test
+# echo  typical
+# python postclassifier.py -v -l typical --train 800 --test 100 --validate 100 > typical.test
