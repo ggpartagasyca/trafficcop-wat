8d6d2daf65567a07b11e391d430938efff51ae03
Andrew Philpot <andrew.philpot@gmail.com>
2015-05-29 18:26:55 -0700

    heres one case that works

--- wat/learn/apply_all.sh
+++ wat/learn/apply_all.sh
@@ -2,42 +2,39 @@
 
 # INPUT='Welcome to Paradise Spa'
 # echo healthspa-default
-# python postclassifier.py -v --name healthspa -i default --load --test 0 --apply "$INPUT"
+# python postclassifier.py -v -n healthspa -i default --load --test 0 --apply "$INPUT"
 
 
-INPUT='<html><body>No black men please</body></html>'
-echo race-default
-python postclassifier.py -v --name race -i ci --load --test 0 --type html --apply "$INPUT"
+# INPUT='<html><body>No black men please</body></html>'
+# echo race-default
+# python postclassifier.py -v -n race -i ci --load --test 0 --type html --apply "$INPUT"
 
+# exit
+
+for fullfile in data/test/*; do
+    filename=$(basename "$fullfile")
+    extension="${filename##*.}"
+    filename="${filename%.*}"
+    if [ "$extension" = "html" ] ;
+    then
+	python postclassifier.py -v -n age -i ci --load --test 0 --type html --apply "`cat $fullfile`"
+    else
+	python postclassifier.py -v -n age -i ci --load --test 0 --type text --apply "`cat $fullfile`"
+    fi
+done
 exit
 
-
-echo age-cs
-python postclassifier.py -v --name age -i cs --load --test 0 --apply "$INPUT"
-# echo race-cs
-# python postclassifier.py -v --name race -i cs --load --test 0 --apply "$INPUT"
-# echo multi-cs
-# python postclassifier.py -v --name multi -i cs --load --test 0 --apply "$INPUT"
-# echo agency-cs
-# python postclassifier.py -v --name agency -i cs --load --test 0 --apply "$INPUT"
-echo healthspa-cs
-python postclassifier.py -v --name healthspa -i cs --load --test 0 --apply "$INPUT"
-# echo offtopic-cs
-# python postclassifier.py -v --name offtopic -i cs --load --test 0 --apply "$INPUT"
-# echo typical-cs
-# python postclassifier.py -v --name typical -i cs --load --test 0 --apply "$INPUT"
-
-# echo age-ci
-# python postclassifier.py -v --name age -i ci --load --test 0 --apply "$INPUT"
-# echo race-ci
-# python postclassifier.py -v --name race -i ci --load --test 0 --apply "$INPUT"
-# echo multi-ci
-# python postclassifier.py -v --name multi -i ci --load --test 0 --apply "$INPUT"
-# echo agency-ci
-# python postclassifier.py -v --name agency -i ci --load --test 0 --apply "$INPUT"
-# echo healthspa-ci
-# python postclassifier.py -v --name healthspa -i ci --load --test 0 --apply "$INPUT"
-# echo offtopic-ci
-# python postclassifier.py -v --name offtopic -i ci --load --test 0 --apply "$INPUT"
-# echo typical-ci
-# python postclassifier.py -v --name typical -i ci --load --test 0 --apply "$INPUT"
+echo age-ci
+python postclassifier.py -v -n age -i ci --load --test 0 --apply "$INPUT"
+echo race-ci
+python postclassifier.py -v -n race -i ci --load --test 0 --apply "$INPUT"
+echo multi-ci
+python postclassifier.py -v -n multi -i ci --load --test 0 --apply "$INPUT"
+echo agency-ci
+python postclassifier.py -v -n agency -i ci --load --test 0 --apply "$INPUT"
+echo healthspa-ci
+python postclassifier.py -v -n healthspa -i ci --load --test 0 --apply "$INPUT"
+echo offtopic-ci
+python postclassifier.py -v -n offtopic -i ci --load --test 0 --apply "$INPUT"
+echo typical-ci
+python postclassifier.py -v -n typical -i ci --load --test 0 --apply "$INPUT"
